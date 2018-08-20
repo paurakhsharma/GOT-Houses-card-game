cdcbb8419dc338a68bf608726325882626caa42e
Paurakh Sharma Humagain <paurakh011@gmail.com>
2018-08-20 22:50:45 +0545

    Bug fixes for double clicks and slow response

--- scripts.js
+++ scripts.js
@@ -1,34 +1,53 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 
 function flipCard() {
+    if (lockBoard) return;
     this.classList.add('flip');
 
     if(!hasFlippedCard) {
         // first click
         hasFlippedCard = true;
         firstCard = this;
-    } else {
-        // second click
-        if(firstCard == this){return}
-        hasFlippedCard = false;
-        secondCard = this;
+        return;
+    } 
+    // second click
+    if (firstCard === this) return; // so when user re-clicks on first one it doesn't count as second
+    secondCard = this;
 
-        // do cards match 
-        if(firstCard.dataset.house === secondCard.dataset.house) {
-            firstCard.removeEventListener('click', flipCard);
-            secondCard.removeEventListener('click', flipCard);
-            console.log('function exexuted')
-        } else {
-            setTimeout(() => {
-                firstCard.classList.remove('flip');
-                secondCard.classList.remove('flip');
-            }, 500)
-        }
-    }
+    checkForMatch();
+}
+
+function checkForMatch() {
+    // do cards match 
+    let isMatch = firstCard.dataset.house === secondCard.dataset.house;
+
+    isMatch ? disableCards() : unFlipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    console.log('function exexuted')
+    resetBoard();
+}
+
+function unFlipCards() {
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetBoard();
+    }, 500)
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
